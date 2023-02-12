b105c1945c6194440e7651108ef7e2224963d44e
kklecho <krzysztof.lechowski@gmail.com>
2023-02-12 15:01:17 +0000

    s3 alias

--- aws/aliases.zsh
+++ aws/aliases.zsh
@@ -3,3 +3,5 @@
 alias ec2down="aws ec2 stop-instances --instance-ids $1"
 alias ec2up="aws ec2 start-instances --instance-ids $1"
 alias creds="code ~/.aws/credentials"
+alias s3="aws s3"
+alias s3ls="aws s3 ls"
