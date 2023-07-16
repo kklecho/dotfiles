5d03ab4206566a5b74e85961fdaf51949c669203
kklecho <krzysztof.lechowski@gmail.com>
2023-07-16 14:23:40 +0100

    imrpove encrypt / decrypt

--- crypt/functions.zsh
+++ crypt/functions.zsh
@@ -4,6 +4,11 @@ function encr_encrypt_file() {
 
     if [[ -z $encrypted_file ]]; then
         encrypted_file=$file.enc
+
+        if [[ -f $encrypted_file ]]; then
+            ts=$(date +"%Y%m%d%H%M%S")
+            encrypted_file=${encrypted_file/.enc/.$ts.enc}
+        fi
     fi
 
     openssl aes-256-cbc -a -salt -pbkdf2 -in $file -out $encrypted_file
@@ -14,7 +19,12 @@ function decr_decrypt_file() {
     local decrypted_file=$2
 
     if [[ -z $decrypted_file ]]; then
-        decrypted_file=$encrypted_file.dec
+        
+        decrypted_file=${encrypted_file/.enc/}
+
+        if [[ -f $decrypted_file ]]; then
+            decrypted_file=$encrypted_file.dec
+        fi
     fi
 
     openssl aes-256-cbc -d -a -pbkdf2 -in $encrypted_file -out $decrypted_file
