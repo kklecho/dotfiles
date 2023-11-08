4a4cc2e23fefc37797b91bf2baae2b23c3349c57
kklecho <krzysztof.lechowski@gmail.com>
2023-11-08 20:41:56 +0000

    hops utils fns

---
+++ hops/functions.zsh
@@ -0,0 +1,65 @@
+function hj() {
+    relpath=$1
+
+    if [ -z "$HOPAPS" ]; then
+        echo "HOPAPS env var must be defined, should point to dir where hop and jl are subdirs"
+        return 1
+    fi
+
+    cd $HOPAPS
+
+    if [ -z "relpath" ]; then
+        echo "Usage: hj <relpath>"
+        return 1
+    fi
+
+    if [ ! -d "hop" ]; then
+        echo "hop dir not found exists"
+        return 1
+    fi
+
+    if [ ! -d "jl" ]; then
+        echo "jl dir not found exists"
+        return 1
+    fi
+
+    if [ ! -f "hop/$relpath" ]; then
+        echo "hop dir not found exists"
+        return 1
+    fi
+
+    cp jl/$relpath hop/$relpath
+}
+
+function jh() {
+    relpath=$1
+
+    if [ -z "$HOPAPS" ]; then
+        echo "HOPAPS env var must be defined, should point to dir where hop and jl are subdirs"
+        return 1
+    fi
+
+    cd $HOPAPS
+
+    if [ -z "relpath" ]; then
+        echo "Usage: hj <relpath>"
+        return 1
+    fi
+
+    if [ ! -d "hop" ]; then
+        echo "hop dir not found exists"
+        return 1
+    fi
+
+    if [ ! -d "jl" ]; then
+        echo "jl dir not found exists"
+        return 1
+    fi
+
+    if [ ! -f "jl/$relpath" ]; then
+        echo "hop dir not found exists"
+        return 1
+    fi
+
+    cp jl/$relpath hop/$relpath
+}
