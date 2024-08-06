fafd8040f5bd665cbd9faa44126480503443268b
kklecho <krzysztof.lechowski@gmail.com>
2024-08-06 09:36:48 +0100

    comment

--- video/functions.zsh
+++ video/functions.zsh
@@ -2,7 +2,7 @@
 function video_to_first_frame(){
     input_file_path=$1
     output_file_path_png=$2
-
+    # 34 refers to which frame to extract, indexed from 0
     ffmpeg -i $input_file_path -vf "select=eq(n\,34)" -vframes 1 $output_file_path_png
 }
 
