108d62f41bc684e5a85e696e1e8cbc7e54aadfa9
kklecho <krzysztof.lechowski@gmail.com>
2024-10-12 14:33:48 +0100

    alias to start-stop lab instance

--- aws/aliases.zsh
+++ aws/aliases.zsh
@@ -7,3 +7,5 @@ alias s3="aws s3"
 alias s3ls="aws s3 ls"
 alias syn="cdk synth"
 alias dep="cdk deploy"
+alias labiup='aws ec2 start-instances --instance-ids $LAB_EC2_INSTANCE'
+alias labidown='aws ec2 stop-instances --instance-ids $LAB_EC2_INSTANCE'
