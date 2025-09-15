86071d8014b0ee1720d129ef2144a87329b5e8c2
kklecho <krzysztof.lechowski@gmail.com>
2025-09-15 10:21:24 +0100

    fix aliases

--- hops/aliases.zsh
+++ hops/aliases.zsh
@@ -1,3 +1,3 @@
-hjgst-jh-git-staged='for f in $(git diff --name-only --cached); do hj $f; done'
-jhgst-jh-git-staged='for f in $(git diff --name-only --cached); do jh $f; done'
+alias hjgst-jh-git-staged='for f in $(git diff --name-only --cached); do hj $f; done'
+alias jhgst-jh-git-staged='for f in $(git diff --name-only --cached); do jh $f; done'
 
