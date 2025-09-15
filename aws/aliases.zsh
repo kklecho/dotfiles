a9f3b06b019b732a6b83bb4e8f746f64c53bb1e7
kklecho <krzysztof.lechowski@gmail.com>
2025-09-15 10:01:46 +0100

    fix creds alias

--- aws/aliases.zsh
+++ aws/aliases.zsh
@@ -3,7 +3,7 @@ alias awsprofile="export AWS_PROFILE="
 #ec2
 alias ec2down="aws ec2 stop-instances --instance-ids $1"
 alias ec2up="aws ec2 start-instances --instance-ids $1"
-alias creds="code ~/.aws/credentials"
+alias creds="e ~/.aws/credentials"
 alias s3="aws s3"
 alias s3ls="aws s3 ls"
 alias syn="cdk synth"
