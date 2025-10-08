54f726ef4a55cc2a6478cfc017e5dc967d2f5e79
kklecho <krzysztof.lechowski@gmail.com>
2025-10-08 08:51:13 +0100

    docker compose aliases fix

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,14 +1,12 @@
 alias d='docker $*'
-alias dc='docker-compose $*'
+alias dc='docker compose $*'
 
-alias dcu="docker-compose up -d"
-alias dcs="docker-compose stop"
-alias dcb="docker-compose build"
-alias dce='docker-compose exec'
-alias dcl='docker-compose logs'
-alias dcps='docker-compose ps'
-alias docker_daemon_stop='sudo snap stop docker'
-alias docker_daemon_start='sudo snap start docker'
+alias dcu="docker compose up -d"
+alias dcs="docker compose stop"
+alias dcb="docker compose build"
+alias dce='docker compose exec'
+alias dcl='docker compose logs'
+alias dcps='docker compose ps'
 
 alias docker_clean_images='docker rmi $(docker images -a --filter=dangling=true -q)'
 alias docker_clean_ps='docker rm $(docker ps --filter=status=exited --filter=status=created -q)'
