dda6250ce59afca92cee0f8402d03e731bc62c34
AUTHORNAME <AUTHOREMAIL>
2026-04-06 22:46:36 +0100

    gbmods alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -20,6 +20,7 @@ alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gbmods="git diff --name-only $(git merge-base main HEAD)"
 alias gac='git add -A && git commit -m'
 alias ge='git-edit-new'
 
