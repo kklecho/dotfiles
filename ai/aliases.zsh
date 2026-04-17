ad05ac177076446d1a9fccddab3bda6d3fc0ed76
AUTHORNAME <AUTHOREMAIL>
2026-04-17 09:28:28 +0100

    new vibe alias

--- ai/aliases.zsh
+++ ai/aliases.zsh
@@ -2,6 +2,7 @@ alias a='vibe'
 alias viber='vibe -c'
 alias agr='source .ai/.env; agr-agent-rebuild'
 alias vip='vibe -p'
+alias vipc='vibe -c -p'
 alias vigo='vibe -p go'
 alias vifb='vibe -p fb'
 
