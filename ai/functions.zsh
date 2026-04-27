2afbdcc747edc53eb815adc5e0a9b31d3759e247
AUTHORNAME <AUTHOREMAIL>
2026-04-27 09:48:23 +0100

    fix: feedback file not working

--- ai/functions.zsh
+++ ai/functions.zsh
@@ -46,8 +46,8 @@ function vibe-apply-feedback() {
 
   for f in $(ls ~/reviewer-feedback-*${hsh}*/*.txt | head -1)
   do
-    vibe -p "Apply review feedback provided in @$f"
     mv $f ./.feedback.txt
+    vibe -p "Apply review feedback provided in @./.feedback.txt"
   done
 }
 
