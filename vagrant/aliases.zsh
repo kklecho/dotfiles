c92e0398d3c874d76d89a8f4f010c60931aff0fd
Krzysztof Lechowski <krzysztof.lechowski@gmail.com>
2026-08-19 10:34:36 +0100

    vag aliases

--- vagrant/aliases.zsh
+++ vagrant/aliases.zsh
@@ -3,4 +3,7 @@ alias vu='vagrant up'
 alias vh='vagrant halt'
 alias vsh='vagrant ssh'
 alias vush='vagrant up; vagrant ssh'
-alias vhdestroy='vagrant halt; vagrant destroy'
+alias vdestroy='vagrant halt; vagrant destroy --force'
+alias vrebuild='vagrant halt; vagrant destroy --force; vagrunt up'
+
+
