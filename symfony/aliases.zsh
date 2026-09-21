0238c4bd648b40f7f169f8ba82b4bb2a7c953693
Krzysztof Lechowski <krzysztof.lechowski@gmail.com>
2026-09-21 13:49:11 +0100

    al for symfony server

--- symfony/aliases.zsh
+++ symfony/aliases.zsh
@@ -2,3 +2,5 @@
 alias c="docker-compose exec php_server php bin/console"
 alias scc="docker-compose exec php_server php bin/console cache:clear"
 alias cmp="docker-compose exec php_server composer"
+alias sss='symfony server:start'
+
